db015f66ecb55ca736d063bb0cc17374222d6f26
leovvay <leovvay@gmail.com>
2021-03-16 18:42:48 +0100

    log api key

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const webpack = require('webpack');
 const { default: Shopify } = require('@shopify/shopify-api');
 
-const apiKey =  JSON.stringify(Shopify.Context.API_KEY);
+const apiKey =  JSON.stringify(process.env.SHOPIFY_API_KEY);
 
 console.log('apikeyfromwindow:', apiKey)
 
